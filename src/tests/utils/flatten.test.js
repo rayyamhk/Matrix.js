11b7bac0550788675a50142488169ef66ebc463c
Ray Yam <rayyamhk@gmail.com>
2021-08-21 17:24:39 +0800

    add more tests

--- src/tests/utils/flatten.test.js
+++ src/tests/utils/flatten.test.js
@@ -12,6 +12,26 @@ describe('should get correct array', () => {
   });
 
   it('TEST 2', () => {
+    const matrix = new Matrix([
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+    ]);
+    const received = matrix.flatten();
+    expect(received).toStrictEqual([1,2,3,4,5,6,7,8]);
+  });
+
+  it('TEST 3', () => {
+    const matrix = new Matrix([
+      [1, 2],
+      [3, 4],
+      [5, 6],
+      [7, 8],
+    ]);
+    const received = matrix.flatten();
+    expect(received).toStrictEqual([1,2,3,4,5,6,7,8]);
+  });
+
+  it('TEST 4', () => {
     const matrix = new Matrix([]);
     const received = matrix.flatten();
     expect(received).toStrictEqual([]);
